86e328cd3fb3aa458a6098b708e3f8db43c5a7a1
Anhith97 <anhithdineshp97@outlook.com>
2024-11-29 14:19:18 +0100

    Update10

--- character.js
+++ character.js
@@ -1,60 +1,65 @@
+let x = 450;
+let y = 450;
+push();
+translate(x, y);
 //The character
 strokeWeight(1);
 background(255, 255, 255);
 //The hat
 fill(255, 255, 255);
-triangle(435, 440, 450, 415, 465, 440);
+triangle(x - 15, y - 10, x, y - 35, x + 15, y - 10);
 fill(255, 0, 0);
-ellipse(450, 450, 35);
-ellipse(450, 418, 10);
+ellipse(x, y, 35);
+ellipse(x, y - 32, 10);
 fill(0, 0, 0);
-ellipse(438, 446, 5);
-ellipse(445, 441, 5);
-ellipse(454, 441, 5);
-ellipse(461, 446, 5);
+ellipse(x - 12, y - 5, 5);
+ellipse(x - 5, y - 9, 5);
+ellipse(x + 4, y - 9, 5);
+ellipse(x + 11, y - 5, 5);
 //The Body
 fill(255, 255, 255);
-rect(436, 475, 30, 35, 5);
-rect(462, 475, 10, 15, 5);
-rect(429, 475, 10, 15, 5);
-line(442, 475, 450, 480);
-line(457, 475, 450, 480);
-line(450, 480, 450, 505);
-ellipse(450, 481, 3);
-ellipse(450, 490, 3);
-ellipse(450, 499, 3);
+rect(x - 14, y + 25, 30, 35, 5);
+rect(x + 12, y + 25, 10, 15, 5);
+rect(x - 21, y + 25, 10, 15, 5);
+line(x - 8, y + 25, x, y + 30);
+line(x + 7, y + 25, x, y + 30);
+line(x, y + 30, x, y + 55);
+ellipse(x, y + 31, 3);
+ellipse(x, y + 40, 3);
+ellipse(x, y + 49, 3);
 //Hands
 fill(0, 0, 0);
-ellipse(434, 507, 10, 12);
-ellipse(467, 507, 10, 12);
+ellipse(x - 16, y + 57, 10, 12);
+ellipse(x + 17, y + 57, 10, 12);
 fill(255, 200, 150);
-rect(429, 487, 10, 17, 1);
-rect(462, 487, 10, 17, 1);
+rect(x - 21, y + 37, 10, 17, 1);
+rect(x + 12, y + 37, 10, 17, 1);
 //The Face
 fill(255, 200, 150);
-ellipse(433, 460, 5, 10);
-ellipse(467, 460, 5, 10);
-ellipse(450, 460, 33);
+ellipse(x - 17, y + 10, 5, 10);
+ellipse(x + 17, y + 10, 5, 10);
+ellipse(x, y + 10, 33);
 fill(0, 0, 0);
-arc(450, 465, 8, 5, 0, 3.14);
+arc(x, y + 15, 8, 5, 0, 3.14);
 //The Eyes
 fill(255, 255, 255);
-ellipse(445, 458, 5, 2);
-ellipse(455, 458, 5, 2);
+ellipse(x - 5, y + 8, 5, 2);
+ellipse(x + 5, y + 8, 5, 2);
 fill(0, 0, 0);
-ellipse(445, 458, 1);
-ellipse(455, 458, 1);
+ellipse(x - 5, y + 8, 1);
+ellipse(x + 5, y + 8, 1);
 //The Shoes
 fill(0, 0, 0);
-ellipse(444, 540, 15, 20);
-ellipse(458, 540, 15, 20);
+ellipse(x - 6, y + 90, 15, 20);
+ellipse(x + 8, y + 90, 15, 20);
 //jeans
 fill(255, 0, 0);
-rect(436, 505, 15, 35, 1);
-rect(451, 505, 15, 35, 1);
-line(445, 505, 436, 515);
-line(456, 505, 465, 515);
+rect(x - 14, y + 55, 15, 35, 1);
+rect(x + 1, y + 55, 15, 35, 1);
+line(x - 5, y + 55, 436, 515);
+line(x + 6, y + 55, 465, 515);
 //The skate
 fill(255, 255, 255);
-rect(439, 547, 10, 27, 3);
-rect(453, 547, 10, 27, 3);
+rect(x - 11, y + 97, 10, 27, 3);
+rect(x + 3, y + 97, 10, 27, 3);
+pop();
